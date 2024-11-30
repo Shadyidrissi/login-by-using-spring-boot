0c4d8c90cf105bb580320222a5b324a702aa0d07
Shady Idrissi <chadiidac@gmail.com>
2024-11-30 12:40:54 +0100

    add check for pass and name not empty and make the new user save

--- src/main/java/com/example/demo/module/Users.java
+++ src/main/java/com/example/demo/module/Users.java
@@ -14,8 +14,17 @@ public class Users {
 
     private String name;
     private String email;
+    private String password;
 
-    // Getters and Setters
+    public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// Getters and Setters
     public Long getId() {
         return id;
     }
